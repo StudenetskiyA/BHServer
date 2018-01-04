316fba2262188fbdf81c4ed4c5049d4f00010f81
StudenetskiyA <wow.dayre@gmail.com>
2018-01-05 02:12:31 +0500

    Kotlin to Java(((

--- src/ru/studenetskiy/model/Main.java
+++ src/ru/studenetskiy/model/Main.java
@@ -1,8 +1,4 @@
-package ru.berserk.model;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.concurrent.ThreadLocalRandom;
+package ru.studenetskiy.model;
 
 public class Main {
 //    static final int COIN_START = 0;
@@ -12,67 +8,5 @@ public class Main {
     static final String url = "jdbc:mysql://localhost:3306/users?useUnicode=yes&characterEncoding=utf8";
     static final String user = "root";
     static final String password = "4lifewithBerserk";
-    static final int COIN_START = 10;
-    
-    static final String CLIENT_VERSION = "0.02";
-    static final String SERVER_VERSION = "0.0311522";
-    public static int randomNum = ThreadLocalRandom.current().nextInt(100, 999 + 1);
-    private static ArrayList<String> names = new ArrayList<>();
-    private static ArrayList<Gamer> freePlayer = new ArrayList<>();
-
-    public static Gamer start(ServerEndpointDemo server) {
-        return new Gamer(server);
-    }
-
-    public static void removeFreePlayer(Gamer g) {
-        synchronized (freePlayer) {
-            freePlayer.remove(g);
-        }
-    }
-
-    public static void addFreePlayer(Gamer g) {
-        synchronized (freePlayer) {
-            freePlayer.add(g);
-        }
-    }
-
-    public static boolean findFreePlayerFor(Gamer g) throws IOException {
-        synchronized (freePlayer) {
-            for (int i = 0; i < Main.freePlayer.size(); i++) {
-                if (!Main.freePlayer.get(i).name.equals(g.name) && Main.freePlayer.get(i).name != null) {
-                    System.out.println("Pair found: " + g.name + "/" + Main.freePlayer.get(i).name);
-                    g.opponent = Main.freePlayer.get(i);
-                    g.opponent.opponent = g;
-                    Main.freePlayer.remove(g);
-                    Main.freePlayer.remove(g.opponent);
-                    g.server.sendMessage("Your opponent " + g.opponent.name + ", play " + g.opponent.player.deck.cards.get(0).name + " hero.");
-        			g.server.sendMessage("$OPPONENTCONNECTED(" + g.opponent.name + "," + g.opponent.player.deck.cards.get(0).name + "," + COIN_START + ")");
-        			g.opponent.server.sendMessage("Your opponent " + g.name + ", play " + g.player.deck.cards.get(0).name + " hero.");
-         			g.opponent.server.sendMessage("$OPPONENTCONNECTED(" + g.name + "," + g.player.deck.cards.get(0).name + "," + COIN_START + ")");
-         			g.isGameStart= true;
-         			g.opponent.isGameStart = true;
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    public static void addName(String name) {
-        synchronized (names) {
-            names.add(name);
-        }
-    }
-
-    public static void removeName(String name) {
-        synchronized (names) {
-            names.remove(name);
-        }
-    }
-
-    public static boolean containsName(String name) {
-        synchronized (names) {
-            return names.contains(name);
-        }
-    }
+   
 }
--- src/ru/studenetskiy/model/TwilightServerEndpoint.java
+++ src/ru/studenetskiy/model/TwilightServerEndpoint.java
@@ -1,4 +1,4 @@
-package ru.berserk.model;
+package ru.studenetskiy.model;
 
 import java.io.IOException;
 
@@ -9,35 +9,27 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
-@ServerEndpoint("/serverendpointdemo")
-public class ServerEndpointDemo {
+@ServerEndpoint("/serverendpoint")
+public class TwilightServerEndpoint {
 
     Session session;
-    Gamer gamer;
 
     @OnOpen
     public void handleOpen(Session session) throws IOException {
         this.session = session;
-        System.out.println("client is now connected");
-        gamer = Main.start(this);
-        gamer.server.sendMessage("Server version is "+Main.SERVER_VERSION);
+        System.out.println("NEW client is now connected");
+        sendMessage("Server online.");
     }
 
     @OnMessage
-    public String handleMessage(String message) {
+    public void handleMessage(String message) throws IOException {
         System.out.println("receive from client: " + message);
-        //String replyMessage = "echo " + message;
-        String replyMessage="";
-        System.out.println("send to client: " + replyMessage);
-        try {
-            gamer.run(message);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return replyMessage;
+        ClientMessageHandler cmh = new ClientMessageHandler(this,message);
+        cmh.proceed();
     }
 
     public void sendMessage(String message) throws IOException {
+    	System.out.println("send to client: " + message);
         this.session.getBasicRemote().sendText(message);
     }
 
@@ -48,13 +40,12 @@ public class ServerEndpointDemo {
     @OnClose
     public void handleClose() throws IOException {
         System.out.println("clien is now disconnected");
-        this.gamer.removePlayer();
     }
 
     @OnError
     public void handleError(Throwable t) {
         System.out.println("error: " + t.getMessage());
-        t.printStackTrace();
+       // t.printStackTrace();
     }
 
 
